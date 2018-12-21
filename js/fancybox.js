c591bdec3a6a923b76113c5381276980e5cbc3c4
markojevtic25 <marko.jevtic2586@gmail.com>
2018-12-21 16:20:30 +0100

    buttons replaced

--- js/fancybox.js
+++ js/fancybox.js
@@ -3,23 +3,13 @@ $(document).ready(function() {
         openEffect	: 'none',
         closeEffect	: 'none'
     });
-
-  /*  $('.fancybox-media').fancybox({
-		openEffect  : 'none',
-		closeEffect : 'none',
-		helpers : {
-			media : {}
+	$(".fancybox-button").fancybox({
+		prevEffect		: true,
+		nextEffect		: true,
+		closeBtn		: true,
+		helpers		: {
+			title	: { type : 'inside' },
+			buttons	: { position: 'top'}
 		}
-	}); */
-	/*$(".various").fancybox({
-		maxWidth	: 800,
-		maxHeight	: 600,
-		fitToView	: false,
-		width		: '70%',
-		height		: '70%',
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});*/
+	});
 });
